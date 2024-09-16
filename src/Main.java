4da1e55216be3db02ae11562652355c15d57ac4e
Santiago <slopez@scl.edu.gt>
2024-09-16 11:29:07 -0600

    trabajo de la rm de LoginControllerselp

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import view.LoginView;
+import controller.LoginController;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+        // Crear la vista del login
+        LoginView loginView = new LoginView();
+        // Crear el controlador y pasarle la vista
+        LoginController loginController = new LoginController(loginView);
+
+        // Mostrar la ventana de login
+        loginView.setVisible(true);
     }
 }
