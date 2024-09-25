bfb38143037b0f81b39bc6fb46e4f9ee06fcbe1e
Santiago <slopez@scl.edu.gt>
2024-09-25 07:50:29 -0600

    a medias del arreglo del main

--- src/Hospital/data/DataDoctor.java
+++ src/Hospital/data/DataDoctor.java
@@ -1,2 +1,27 @@
 package Hospital.data;
+import servicios.BaseDeDatosDoc;
+import servicios.Doctor;
+
+import java.util.ArrayList;
+
+public class DataDoctor {
+    ArrayList<Doctor> data = new ArrayList<>();
+
+    public DataDoctor() {
+
+        data.add(new Doctor("Dr. Juan Kook", "jjkkk.kook@bhospitaldd.com", "Cardiología", "jkkkook", "bag23"));
+        data.add(new Doctor("Dra. Jinna Kikm", "jinna.kikm@bhospitaldd.com", "Dermatología", "jinnakm", "tan43"));
+        data.add(new Doctor("Dr. Ricardo Morales", "ricardo.morales@bhospitaldd.com", "Pediatría", "ricardMo", "bamm56"));
+        data.add(new Doctor("Dra. Agustina Minn", "agustina.minn@bhospitaldd.com", "Traumatología", "agustM", "portyyea43"));
+        data.add(new Doctor("Dr. Tauren huyn", "tauren.huyn@bhospitaldd.com", "Neurología", "taurenh", "niagarapo"));
+
+    }
+
+    public ArrayList<Doctor> getDataDoctor() {
+         return data;
+    }
+}
+
+
+
 
